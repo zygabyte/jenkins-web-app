311eba3c05339d86b2393d4a6b44d6cdf6a108b2
Eseosa <eseosaehioghae@gmail.com>
2019-04-12 12:17:38 +0100

    updates

--- JenkinsWebApp/Controllers/HomeController.cs
+++ JenkinsWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace JenkinsWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. NEW NEW NEWN EWNNE";
 
             return View();
         }
